ae06d82125ec677e1e91e31e713981b401ea58f0
Roman Yurkovskiy <roman.yurkovskiy@gmail.com>
2020-01-21 09:55:28 +0200

    Add new license text for all files

--- BlocksettleNetworkingLib/SettlementTransactionBroadcaster.h
+++ BlocksettleNetworkingLib/SettlementTransactionBroadcaster.h
@@ -1,3 +1,13 @@
+/*
+
+***********************************************************************************
+* Copyright (C) 2016 - , BlockSettle AB
+* Distributed under the GNU Affero General Public License (AGPL v3)
+* See LICENSE or http://www.gnu.org/licenses/agpl.html
+*
+**********************************************************************************
+
+*/
 #ifndef __SETTLEMENT_TRANSACTION_BROADCASTER_H__
 #define __SETTLEMENT_TRANSACTION_BROADCASTER_H__
 
--- BlocksettleNetworkingLib/XBTTradeData.h
+++ BlocksettleNetworkingLib/XBTTradeData.h
@@ -1,3 +1,13 @@
+/*
+
+***********************************************************************************
+* Copyright (C) 2016 - , BlockSettle AB
+* Distributed under the GNU Affero General Public License (AGPL v3)
+* See LICENSE or http://www.gnu.org/licenses/agpl.html
+*
+**********************************************************************************
+
+*/
 #ifndef __XBT_TRADE_DATA_H__
 #define __XBT_TRADE_DATA_H__
 
